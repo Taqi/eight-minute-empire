439a03758ed5e09950d38de5b13a7530a94b3a69
Daphne <daphne.augier@gmail.com>
2019-11-26 02:54:36 -0500

    CMake files
    just for my IDE

---
+++ cmake-build-debug/CMakeFiles/eight_minute_empire.dir/DependInfo.cmake
@@ -0,0 +1,73 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/Bidding/Bidding.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/Bidding/Bidding.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/Bidding/BiddingDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/Bidding/BiddingDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/Card/Cards.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/Card/Cards.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/Card/CardsDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/Card/CardsDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/GameMainLoop/GameMainLoop.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/GameMainLoop/GameMainLoop.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/GameScore/GameScore.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/GameScore/GameScore.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/GameScore/GameScoreDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/GameScore/GameScoreDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/GameState/GameState.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/GameState/GameState.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/GameState/GameStateDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/GameState/GameStateDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/Bidding/Bidding.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/Bidding/Bidding.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/Bidding/BiddingDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/Bidding/BiddingDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/Card/Cards.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/Card/Cards.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/Card/CardsDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/Card/CardsDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/GameMainLoop/GameMainLoop.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/GameMainLoop/GameMainLoop.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/GameScore/GameScore.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/GameScore/GameScore.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/GameScore/GameScoreDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/GameScore/GameScoreDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/GameState/GameState.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/GameState/GameState.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/GameState/GameStateDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/GameState/GameStateDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/Map/Map.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/Map/Map.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/Map/MapDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/Map/MapDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/MapLoader/MapLoader.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/MapLoader/MapLoader.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/MapLoader/MapLoaderDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/MapLoader/MapLoaderDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/Player/Player.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/Player/Player.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/Player/PlayerDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/Player/PlayerDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/StartUpPhase/StartUpPhase.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/StartUpPhase/StartUpPhase.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/StartUpPhase/StartUpPhaseDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/StartUpPhase/StartUpPhaseDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/James's Part 2/main.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/James's_Part_2/main.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/Map/Map.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/Map/Map.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/Map/MapDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/Map/MapDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/MapLoader/MapLoader.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/MapLoader/MapLoader.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/MapLoader/MapLoaderDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/MapLoader/MapLoaderDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/Player/Player.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/Player/Player.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/Player/PlayerDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/Player/PlayerDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/StartUpPhase/StartUpPhase.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/StartUpPhase/StartUpPhase.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/StartUpPhase/StartUpPhaseDriver.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/StartUpPhase/StartUpPhaseDriver.cpp.o"
+  "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/main.cpp" "/Users/daphneaugier/Desktop/Coding/eight-minute-empire/cmake-build-debug/CMakeFiles/eight_minute_empire.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Bidding"
+  "../Card"
+  "../GameMainLoop"
+  "../GameScore"
+  "../GameState"
+  "../James's Part 2/Bidding"
+  "../James's Part 2/Card"
+  "../James's Part 2/GameMainLoop"
+  "../James's Part 2/GameScore"
+  "../James's Part 2/GameState"
+  "../James's Part 2/Map"
+  "../James's Part 2/MapLoader"
+  "../James's Part 2/Player"
+  "../James's Part 2/StartUpPhase"
+  "../Map"
+  "../MapLoader"
+  "../Player"
+  "../StartUpPhase"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
